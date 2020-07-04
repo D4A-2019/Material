014bd74408e15b973bc2667c8c8a0bced3d001ef
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-07-04 16:16:02 +0700

    add react navigation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* Rest of your app code */}
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
