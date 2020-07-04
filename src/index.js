271c1f8c8187c30c9501bc61c49226ac3be6cecc
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-07-04 16:50:15 +0700

    stack and button

--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
 
-export default function App() {
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({navigation}) {
   return (
-    <NavigationContainer>
-      {/* Rest of your app code */}
-    </NavigationContainer>
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+    // <View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button mode="contained" onPress={() => navigation.navigate('Materi')}>Materi</Button>
+    </View>
   );
 }
+
+function MaterialsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    // <View style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }}/>
+          <Stack.Screen name="Materi" component={MaterialsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      // {/* <Text>Open up App.js to start working on your app!</Text> */}
+      // {/* <StatusBar style="auto" />
+    // </View> */}
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
