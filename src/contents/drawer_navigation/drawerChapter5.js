3164020145b7a51efd9db312c965e57c13c00d37
Aldy Akbarrizky <AldyAkbarrizky18@gmail.com>
2020-07-24 10:53:47 +0700

    Fix Title in Drawer Navigation

--- src/contents/drawer_navigation/drawerChapter5.js
+++ src/contents/drawer_navigation/drawerChapter5.js
@@ -7,22 +7,22 @@ const Drawer = createDrawerNavigator();
 export default class drawerChapter5 extends Component {
     render() {
         return(
-            <Drawer.Navigator initialRouteName="Chapter5">
-                <Drawer.Screen name="Chapter1" component={Chapter.Chapter1}/>
-                <Drawer.Screen name="Chapter2" component={Chapter.Chapter2}/>
-                <Drawer.Screen name="Chapter3" component={Chapter.Chapter3}/>
-                <Drawer.Screen name="Chapter4" component={Chapter.Chapter4}/>
-                <Drawer.Screen name="Chapter5" component={Chapter.Chapter5}/>
-                <Drawer.Screen name="Chapter6" component={Chapter.Chapter6}/>
-                <Drawer.Screen name="Chapter7" component={Chapter.Chapter7}/>
-                <Drawer.Screen name="Chapter8" component={Chapter.Chapter8}/>
-                <Drawer.Screen name="Chapter9" component={Chapter.Chapter9}/>
-                <Drawer.Screen name="Chapter10" component={Chapter.Chapter10}/>
-                <Drawer.Screen name="Chapter11" component={Chapter.Chapter11}/>
-                <Drawer.Screen name="Chapter12" component={Chapter.Chapter12}/>
-                <Drawer.Screen name="Chapter13" component={Chapter.Chapter13}/>
-                <Drawer.Screen name="Chapter14" component={Chapter.Chapter14}/>
-                <Drawer.Screen name="Chapter15" component={Chapter.Chapter15}/>
+            <Drawer.Navigator initialRouteName="BAB 5">
+                <Drawer.Screen name="BAB 1" component={Chapter.Chapter1}/>
+                <Drawer.Screen name="BAB 2" component={Chapter.Chapter2}/>
+                <Drawer.Screen name="BAB 3" component={Chapter.Chapter3}/>
+                <Drawer.Screen name="BAB 4" component={Chapter.Chapter4}/>
+                <Drawer.Screen name="BAB 5" component={Chapter.Chapter5}/>
+                <Drawer.Screen name="BAB 6" component={Chapter.Chapter6}/>
+                <Drawer.Screen name="BAB 7" component={Chapter.Chapter7}/>
+                <Drawer.Screen name="BAB 8" component={Chapter.Chapter8}/>
+                <Drawer.Screen name="BAB 9" component={Chapter.Chapter9}/>
+                <Drawer.Screen name="BAB 10" component={Chapter.Chapter10}/>
+                <Drawer.Screen name="BAB 11" component={Chapter.Chapter11}/>
+                <Drawer.Screen name="BAB 12" component={Chapter.Chapter12}/>
+                <Drawer.Screen name="BAB 13" component={Chapter.Chapter13}/>
+                <Drawer.Screen name="BAB 14" component={Chapter.Chapter14}/>
+                <Drawer.Screen name="BAB 15" component={Chapter.Chapter15}/>
             </Drawer.Navigator>
         );
     }
